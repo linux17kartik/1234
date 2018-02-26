583885879c18e9a44441f4aa02b5812966d2e5c7
kartik <34565796+linux17kartik@users.noreply.github.com>
2018-02-26 15:57:56 +0530

    Add files via upload

--- test1/server.js
+++ test1/server.js
@@ -2,16 +2,35 @@
 
 const express = require('express');
 
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'mysql#123',
+  database : 'my_db'
+});
+ 
+connection.connect();
+
 // Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
+const PORT = 80;
 
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  res.send('Hello kartik world\n');
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(PORT);
+console.log(`Running on http://:${PORT}`);
+
+ 
+connection.query('SELECT * from Persons', function (error, results, fields) {
+  if (error){
+	console.log(error);	
+	}
+  console.log('The solution is: ', results);
+});
+ 
+connection.end();
 
